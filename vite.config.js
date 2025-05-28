9d8c8700c1f861d3873386ae46f6eef24e5650e1
Sidrajagrawal <142567526+Sidrajagrawal@users.noreply.github.com>
2025-05-28 15:16:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: ['e253-2405-201-3046-f012-e96e-5fe1-d4e3-a59a.ngrok-free.app']
-  }
 })
